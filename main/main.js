90264cf39b77399e309a2081bf3019fdaf0104ad
mrxinxie <mrxinxie@outlook.com>
2018-09-25 22:28:14 +0800

    add main

--- main/main.js
+++ main/main.js
@@ -1,18 +1,31 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.total()}
+o) 平均值 = ${sequence.ave().toFixed(2)}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+	this.input = input;
+	this.input = this.input.sort();
   }
 
   minimum() {
-    // Write your code here
+	return this.input[0]
   }
-
-  // Write your code here
+  maximum() {
+	  return this.input[this.input.length - 1];
+  }
+  total() {
+	  return this.input.length;
+  }
+  ave() {
+	  var s = 0;
+	  for(var key of this.input) {
+		 s = s + key; 
+	  }
+	  return s / this.input.length;
+  }
 }
